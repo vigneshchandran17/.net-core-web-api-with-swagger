9f608cc71d56d18a5079d21d94ecfe5d1856a757
vignesh <vignesh@descpro.com>
2023-02-12 09:54:29 +0530

    Initial Commit

---
+++ sample web api with swagger/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sample_web_api_with_swagger.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("/api/teststring")]
+        public string GetString()
+        {
+            return "Hello World";
+        }
+    }
+}
